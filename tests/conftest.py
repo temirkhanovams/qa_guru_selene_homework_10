b8fac35159148c6e91494af772d1f1baa15851e3
temirkhanovams <marinchik.t.s@gmail.com>
2024-03-30 23:24:30 +0300

    init

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+from selene.support.shared import browser
+import pytest
+
+
+@pytest.fixture(scope='function', autouse=True)
+def browser_init():
+    print('\nОткрываю браузер')
+    browser.config.window_width = 1024
+    browser.config.window_height = 780
+    # browser.open("https://demoqa.com/automation-practice-form")
+    browser.get("https://demoqa.com/automation-practice-form")
+
+    yield
+
+    print('\nЗакрываю браузер')
+    browser.quit()
