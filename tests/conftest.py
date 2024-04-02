b21abce3c92a8078cf61e289fc37267f10497826
temirkhanovams <marinchik.t.s@gmail.com>
2024-04-02 15:23:39 +0300

    Доработала ожидание с помощью page_load_strategy = 'eager'

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 from selene import browser
 import pytest
+from selenium import webdriver
 
 
 @pytest.fixture(scope='function')
@@ -7,6 +8,9 @@ def browser_init():
     browser.config.base_url = 'https://demoqa.com'
     browser.config.window_width = 1024
     browser.config.window_height = 780
+    driver_options = webdriver.ChromeOptions()
+    driver_options.page_load_strategy = 'eager'
+    browser.config.driver_options = driver_options
 
     yield
 
